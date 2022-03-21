81b2bd6937f63ee43e90e33388d54815cc751933
viondeveloper <hiepnmdev@gmail.com>
2022-03-21 11:18:02 +0700

    Initial Commit

---
+++ d1.js
@@ -0,0 +1,13 @@
+// // var nhapten = window.prompt("nhap ten", nhapten);
+// // var nhaptuoi = window.prompt("nhap tuoi", nhaptuoi);
+
+// // nhapten = document.write(`chao ban: <b>${nhapten}</b>`);
+// // document.write("<br>");
+// // nhaptuoi = document.write(`tuoi cua ban la: <u>${nhaptuoi}</u>`);
+
+// function myFunction(){
+//     // document.getElementsByName("welcome").onclick = alert("welcome to javascript");
+//     document.getElementsByName("msg")
+    
+// }
+
